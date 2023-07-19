1e5b33bbf959cd92cf7ce34e3592867d2ace4721
JENIPHER123 <jeniperkuki@gmail.com>
2023-07-19 15:57:04 +0300

    adding links and routes

--- twitter/test/widget_test.dart
+++ twitter/test/widget_test.dart
@@ -8,8 +8,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:twitter/landing/landing.dart';
-
-import 'package:twitter/main.dart';
+/* 
+import 'package:twitter/main.dart'; */
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
